3c59074bf8112618473cbd3935fe955c7cb3b11b
Asif Salam <asif.salam@hotmail.com>
2023-08-08 15:30:28 +0200

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('vite').UserConfig} */
 const config = {
+	kit: {
+		adapter: adapter(),
+		target: '#svelte'
+	},
 	plugins: [sveltekit()]
 };
 
