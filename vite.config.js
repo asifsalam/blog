87bba0708c31af2f7ca6332450250d08cff7b25d
Asif Salam <asif.salam@hotmail.com>
2023-08-08 17:33:58 +0200

    Remove adapter from vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-vercel';
+
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		target: '#svelte'
-	},
 	plugins: [sveltekit()]
 };
 
